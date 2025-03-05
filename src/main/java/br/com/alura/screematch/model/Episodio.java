b699c7badb9c674ecd81b72dc0716f6704149982
Weverton Matias <weverton-matias@hotmail.com>
2025-03-05 16:11:53 -0300

    Manipulando com fluxo as colecoes de dados.

---
+++ src/main/java/br/com/alura/screematch/model/Episodio.java
@@ -0,0 +1,79 @@
+package br.com.alura.screematch.model;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+
+    private Integer numeroTemporada;
+    private String titulo;
+    private Integer numero;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer temporada, DadosEpisodio episodio) {
+        this.numeroTemporada = temporada;
+        this.titulo = episodio.titulo();
+        this.numero = episodio.numero();
+        try {
+            this.avaliacao = Double.valueOf(episodio.avaliacao());
+        } catch (NumberFormatException e) {
+            this.avaliacao = 0.0;
+        }
+
+        try {
+            this.dataLancamento = LocalDate.parse(episodio.dataLancamento());
+        }catch (DateTimeParseException e) {
+            this.dataLancamento = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "Temporada: %s | Episodio: %s | Avaliação %s | Data de Lançamento %s | Titulo: %s"
+                .formatted(this.numeroTemporada, this.numero, this.avaliacao, formatter.format(this.dataLancamento), this.titulo);
+    }
+
+    public Integer getNumeroTemporada() {
+        return numeroTemporada;
+    }
+
+    public void setNumeroTemporada(Integer numeroTemporada) {
+        this.numeroTemporada = numeroTemporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+}
--- src/main/java/br/com/alura/screematch/principal/Principal.java
+++ src/main/java/br/com/alura/screematch/principal/Principal.java
@@ -3,12 +3,16 @@ package br.com.alura.screematch.principal;
 import br.com.alura.screematch.model.DadosEpisodio;
 import br.com.alura.screematch.model.DadosSerie;
 import br.com.alura.screematch.model.DadosTemporada;
+import br.com.alura.screematch.model.Episodio;
 import br.com.alura.screematch.service.ConsumoApi;
 import br.com.alura.screematch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -50,7 +54,37 @@ public class Principal {
 //                )
 //        );
 
-        temporadas.forEach(t -> t.episodios().forEach(System.out::println));
+        //temporadas.forEach(t -> t.episodios().forEach(System.out::println));
 
+        List<DadosEpisodio> dadosEpisodio = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+//                .toList();
+//        dadosEpisodio.add(new DadosEpisodio("teste", 5, "10", "2020-01-01"));
+//        dadosEpisodio.forEach(System.out::println);
+
+        System.out.println("Top 5 Episodios:");
+        dadosEpisodio.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5);
+//                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d)
+                        )
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano voce deseja ve os episodios da serie? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(System.out::println);
     }
 }
